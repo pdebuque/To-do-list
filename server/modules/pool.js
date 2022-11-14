4db511789b95ac3edb923e4f57457e6e75509347
Paolo Debuque <pdebuque@gmail.com>
2022-11-14 15:51:02 -0600

    tweak

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,12 +1,31 @@
 const pg = require('pg');
-const Pool = pg.Pool;
-const pool = new Pool({
-    database: 'to-do list', // name of db; this can change!
-    host: 'localhost', // where is your db?
-    port: 5432, // default for Postgres
-    max: 10, // max queries at one time
-    idleTimeoutMillis: 30000 // 30 seconds to try to connect, otherwise cancel query
-});
+let pool;
+
+// const Pool = pg.Pool;
+// const pool = new Pool({
+//     database: 'to-do-list', // name of db; this can change!
+//     host: 'localhost', // where is your db?
+//     port: 5432, // default for Postgres
+//     max: 10, // max queries at one time
+//     idleTimeoutMillis: 30000 // 30 seconds to try to connect, otherwise cancel query
+// });
+
+if (process.env.DATABASE_URL) { // if we are on heroku, there will be a process.env.DATABASE_URL
+    pool = new pg.Pool({
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+            rejectUnauthorized: false
+        }
+    });
+} else {
+    pool = new pg.Pool({
+        host: 'localhost',
+        port: 5432,
+        database: 'to-do list',
+        max: 10,
+        idleTimeoutMillis: 30000
+    });
+}
 
 // not required but useful for debugging
 pool.on('connect', () => {
